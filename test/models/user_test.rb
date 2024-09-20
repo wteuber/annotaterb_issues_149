fd28325e6c57966fcc83b59ed5106cfca308fdd6
Wolfgang Teuber <wolfgang.teuber@shopify.com>
2024-09-20 20:03:13 +0100

    Create test cases to use the fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user count" do
+    assert_equal 100, User.count
+  end
+
+  test "first user" do
+    assert_equal "User 1", users(:user_1).name
+    assert_equal "user_1@example.com", users(:user_1).email
+  end
 end
